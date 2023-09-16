0c27382f5074373d385ff878ffbca81a06dd3df5
harshita <harshitakatara34>
2023-09-16 14:43:07 +0530

    d

--- Functionreturningarray.js
+++ Functionreturningarray.js
@@ -6,18 +6,35 @@ function useState(num) {
     }
   
     return [count, setCount]
+    // [20,function]
   }
   
   // way 1
   let resultArr = useState(20);
+//   [20,function]
+  console.log(resultArr)
+
   let resultCount = resultArr[0];
+  console.log(resultCount)
   let resultSetFunction = resultArr[1];
+  console.log(resultSetFunction())
   
-  resultSetFunction(); //-> setting the count: 20
+//   resultSetFunction(); //-> setting the count: 20
+
+
+
+
   
+
+
+
+
 // Way2
+// Destructuring can be done in function also 
 
   // the destructuring way, super neat
-  let [count, setCount] = useState(30);
+//   let [count, setCount] = useState(30);
   
-  setCount(); //-> setting the count: 30
+//   setCount(); //-> setting the count: 30
+console.log(count)
+
--- This.js
+++ This.js
@@ -1,13 +1,22 @@
 const uh1 ={
     name: 'ghi',
+    temp:function(){
+      console.log(this)
+    },
     nest:{
       age: 23,
+      tempo:function(){
+        console.log(this)
+      },
       edica: {
+        name:"harshi",
         mainfun : function(){
           console.log(this, `My age is 23`)  // 'My age is 23'
         }
       }
     }
   }
+  console.log(uh1.nest.tempo())
   
   console.log(uh1.nest.edica.mainfun())
+  console.log(uh1.temp())
