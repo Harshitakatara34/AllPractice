ec73a75d38c18f29d33bc2cde62c7ce462139829
harshita <harshitakatara34>
2023-09-16 10:10:52 +0530

    d

---
+++ Bigint.js
@@ -0,0 +1,5 @@
+// the "n" at the end means it's a BigInt
+const aVeryBigNumber = 1234567890123456789012345678901234567890n;
+console.log(typeof aVeryBigNumber)
+
+console.log(1.2345678901234568*(10**39))
