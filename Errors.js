cf2c2efc5966c8cde8939f56fbe886b1ccafae10
harshita <harshitakatara34>
2023-09-16 20:41:27 +0530

    hoisting

---
+++ Errors.js
@@ -0,0 +1,14 @@
+// TypeError
+// const a=20
+// a=10
+// console.log(a) // give type Error 
+
+
+// Reference Error 
+// Whenever the compiler will atry to access the variable from memeory from where it can't be able to accessed
+// then it will show refrence Error (temporal Dead Zone)
+
+//Syntax Error
+// let a=20
+// let a ;
+// console.log(a)
