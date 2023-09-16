86e23b7e96ac7bbe8d3a31bd4dd89eaf70011693
harshita <harshitakatara34>
2023-09-16 13:43:18 +0530

    contructor

---
+++ ImpExport.js
@@ -0,0 +1,10 @@
+ // Export
+ // named exports
+export { Teacher, Engineer }
+
+// default export
+export default Person;
+
+
+// Imports
+import Person, {Teacher, Engineer} from "./local_modules/Person.js";
