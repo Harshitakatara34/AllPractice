91604a070ca9c8c0edfb0328cc1c8d5083487878
harshita <harshitakatara34>
2023-09-17 12:00:12 +0530

    prim

--- Functionreturningarray.js
+++ Functionreturningarray.js
@@ -1,25 +1,25 @@
-// a function that returns an array of a number and a function
-function useState(num) {
-    let count = num;
-    let setCount = function () {
-      console.log(`setting the count: ${count}`);
-    }
+// // a function that returns an array of a number and a function
+// function useState(num) {
+//     let count = num;
+//     let setCount = function () {
+//       console.log(`setting the count: ${count}`);
+//     }
   
-    return [count, setCount]
-    // [20,function]
-  }
+//     return [count, setCount]
+//     // [20,function]
+//   }
   
-  // way 1
-  let resultArr = useState(20);
-//   [20,function]
-  console.log(resultArr)
+//   // way 1
+//   let resultArr = useState(20);
+// //   [20,function]
+//   console.log(resultArr)
 
-  let resultCount = resultArr[0];
-  console.log(resultCount)
-  let resultSetFunction = resultArr[1];
-  console.log(resultSetFunction())
+//   let resultCount = resultArr[0];
+//   console.log(resultCount)
+//   let resultSetFunction = resultArr[1];
+//   console.log(resultSetFunction())
   
-//   resultSetFunction(); //-> setting the count: 20
+// //   resultSetFunction(); //-> setting the count: 20
 
 
 
@@ -29,12 +29,12 @@ function useState(num) {
 
 
 
-// Way2
-// Destructuring can be done in function also 
+// // Way2
+// // Destructuring can be done in function also 
 
-  // the destructuring way, super neat
-//   let [count, setCount] = useState(30);
+//   // the destructuring way, super neat
+// //   let [count, setCount] = useState(30);
   
-//   setCount(); //-> setting the count: 30
-console.log(count)
+// //   setCount(); //-> setting the count: 30
+// console.log(count)
 
