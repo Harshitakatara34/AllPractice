f72ee7bd6afe1bbeb1e07edb2a4f0f5c0aee5ff5
harshita <harshitakatara34>
2023-09-18 10:14:18 +0530

    d

---
+++ ClassicalandPrototypicalInheritence.js
@@ -0,0 +1,40 @@
+// // Classical Inheritence 
+// class Animal {
+//     constructor(name) {
+//       this.name = name;
+//     }
+  
+//     speak() {
+//       console.log(this.name + ' makes a sound.');
+//     }
+//   }
+  
+//   class Dog extends Animal {
+//     constructor(name, breed) {
+//       super(name);
+//       this.breed = breed;
+//     }
+  
+//     speak() {
+//       console.log(this.name + ' barks loudly.');
+//     }
+//   }
+  
+//   const dog1 = new Dog('Buddy', 'Golden Retriever');
+//   dog1.speak(); // Output: Buddy barks loudly.
+  
+
+
+
+
+
+
+
+
+//PrototTypical Inheritence
+// let obj1={
+//     name:"temp"
+// }
+// let obj2=Object.create(obj1)
+// obj2.data="d"
+// console.log(obj2.name)
