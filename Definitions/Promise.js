c9d5eeee021a9a50bde7fcb1ffae397dc4f4f1cf
harshita <harshitakatara34>
2023-09-18 11:12:39 +0530

    promise

---
+++ Definitions/Promise.js
@@ -0,0 +1,15 @@
+// Promise are the javascript object that handles asynchronous Operations
+// they have three different states
+// 1. pending
+// 2. fulfilled
+// 3. rejected
+
+let promise = new Promise((res, rej) => {
+  let temp = true;
+  if (temp) {
+    res("resolved");
+  } else {
+    rej("error");
+  }
+});
+console.log(promise)
