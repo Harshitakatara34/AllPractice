69f26f8632a1c2bd60efa670c3176ea93389c6c2
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-09-18 19:20:08 +0530

    done

---
+++ Definitions/DataType.js
@@ -0,0 +1,23 @@
+1.// What is Datatype in javaScript?
+
+// A data type in programming defines the kind of data a variable can hold, specifying its format and allowable operations.
+
+
+//=======================
+
+2.//Primitive Data Types:
+
+// Primitive data types in JavaScript are immutable, simple data types that hold a single value each, such as numbers, strings, and booleans.
+// string, number, boolean, undefined, null, bigInt, symbol
+
+3.//Non-primitive:
+
+//  data types are complex, mutable data structures like objects and arrays that can hold multiple values and are manipulated by reference.
+//object, array, function
+
+
+//============================
+
+4.//## Dynamic Typing
+
+// using the `typeof` operator to check types & change them dynamically (with two exeptions)
---
+++ Samreen Js Practice/VivekSirClass34.js
@@ -0,0 +1,53 @@
+let shapes = [
+    {sides: 4, color: 'red'},
+    {sides: 3, color: 'blue'},
+    {sides: 4, color: 'blue'},
+    {sides: 3, color: 'red'}
+  ];
+  
+//   shapes.sort(function(a, b) {
+//     return a.color.localeCompare(b.color);
+//   });
+
+  shapes.sort((a,b)=>{
+    return b.color.localeCompare(a.color);
+
+  })
+  
+  console.log(shapes);
+
+  // Output: [
+  //   {sides: 3, color: 'blue'},
+  //   {sides: 4, color: 'blue'},
+  //   {sides: 3, color: 'red'},
+  //   {sides: 4, color: 'red'}
+  // ]
+
+//   [
+//     { sides: 4, color: 'red' },
+//     { sides: 3, color: 'red' },
+//     { sides: 3, color: 'blue' },
+//     { sides: 4, color: 'blue' }
+//   ]
+
+
+
+
+// localeCompare
+
+// In the code you provided, localeCompare is a JavaScript method used to compare two strings based on their Unicode values in a locale-sensitive manner. It is often used for sorting strings alphabetically, taking into account language-specific sorting rules and character comparisons.
+
+
+
+//Prototype is a object - chain function to object.
+
+
+let ag = [1,3,4]
+
+console.log(ag)
+
+
+// Object.prototype
+// Array.prototype 
+
+// both are properties
