e3513edf41b71f2249b1dc0b339f423bcf785115
harshita <harshitakatara34>
2023-09-18 17:59:32 +0530

    sort

---
+++ sortingandfiltering.js
@@ -0,0 +1,10 @@
+//Sorting
+// const numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+// numbers.sort((a, b) => a - b);
+// console.log(numbers); // Output: [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
+
+
+//Filtering
+// const fruits = ["apple", "banana", "cherry", "date"];
+// const filteredFruits = fruits.filter((fruit) => fruit !== "banana");
+// console.log(filteredFruits); // Output: ["apple", "cherry", "date"]
