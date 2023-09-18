bd11d144e161f0371c529160fa31a405e5ba064a
harshita <harshitakatara34>
2023-09-18 10:38:14 +0530

    d

--- GettersandSetters.js
+++ GettersandSetters.js
@@ -1,29 +1,62 @@
+// class Person {
+//     constructor(name, age) {
+//       this.name = name;
+//       this.age = age;
+//       this.salary = 5000;
+//     }
+  
+//     // getter
+//     get fullName() {
+//       return `${this.name} Doe`;
+//     }
+  
+//     // setter
+//     set fullName(name) { 
+//         // name=jane Smith
+//       this.name = name.split(" ")[0];
+//     }
+//   }
+  
+//   const john = new Person("John", 30);
+  
+//   // are we trying to get a value or set a value
+//   console.log(john.fullName); // get
+  
+//   // are we trying to get a value or set a value
+//   john.fullName = "Jane Smith"; // set
+  
+//   // get a value or are we trying to get a value
+//   console.log(john.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Person {
+    #age;    ///private variable we can't accesss it outside the class
+  
     constructor(name, age) {
       this.name = name;
-      this.age = age;
-      this.salary = 5000;
+      this.#age = age;
+     
     }
-  
-    // getter
-    get fullName() {
-      return `${this.name} Doe`;
-    }
-  
-    // setter
-    set fullName(name) { 
-        // name=jane Smith
-      this.name = name.split(" ")[0];
-    }
+   
+    get age() {
+        return this.#age;
+
+      }
+     
   }
   
   const john = new Person("John", 30);
-  
-  // are we trying to get a value or set a value
-  console.log(john.fullName); // get
-  
-  // are we trying to get a value or set a value
-  john.fullName = "Jane Smith"; // set
-  
-  // get a value or are we trying to get a value
-  console.log(john.name);
+  console.log(john)
+  console.log(john.age); // 30
+//   console.log(john.#age); // SyntaxError: Private field '#age' must be declared in an enclosing class
