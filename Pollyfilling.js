f65cac0207bead504391958b7d2d3a74ef0d0382
harshita <harshitakatara34>
2023-09-18 18:33:35 +0530

    d

--- Pollyfilling.js
+++ Pollyfilling.js
@@ -1,28 +1,28 @@
 
 
-// Polyfill: Code that adds missing features to older web browsers.
-// - A polyfill is JavaScript code.
-// - It extends a web browser's capabilities.
-// - It enables support for features not natively provided by the browser.
-// - It ensures compatibility with older browsers.
-// - It allows developers to use modern features in older environments.
+// // Polyfill: Code that adds missing features to older web browsers.
+// // - A polyfill is JavaScript code.
+// // - It extends a web browser's capabilities.
+// // - It enables support for features not natively provided by the browser.
+// // - It ensures compatibility with older browsers.
+// // - It allows developers to use modern features in older environments.
 
 
 
 
 
 
-Array.prototype.map = function(cb){
-    return ['Mugambo', 'Khush', 'Huva'];
-  }
+// Array.prototype.map = function(cb){
+//     return ['Mugambo', 'Khush', 'Huva'];
+//   }
   
-  // let arr = ["A", "O", "Suno", "lo", "ha", "na"];
+//   // let arr = ["A", "O", "Suno", "lo", "ha", "na"];
   
-  // let newArr =  arr.map(item => item + ' ji')
-  // console.log(newArr)
+//   // let newArr =  arr.map(item => item + ' ji')
+//   // console.log(newArr)
   
   
-  let newArr3 = [1,2,3,4,5].map(item => item+1);
-  console.log(newArr3);
+//   let newArr3 = [1,2,3,4,5].map(item => item+1);
+//   console.log(newArr3);
   
   
