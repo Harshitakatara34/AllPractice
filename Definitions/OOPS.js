b214104c499b5c66afa11f586c8d9b5d2c2e4db9
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-20 11:30:40 +0530

    d

--- Definitions/OOPS.js
+++ Definitions/OOPS.js
@@ -1,4 +1,34 @@
 
-// JavaScript Object-Oriented Programming (OOP) is a programming approach that employs objects to represent 
-// real-world entities, promoting code organization and reusability through 
-// constructor functions, classes (ES6), and prototype-based inheritance.
+// // JavaScript Object-Oriented Programming (OOP) is a programming approach that employs objects to represent 
+// // real-world entities, promoting code organization and reusability through 
+// // constructor functions, classes (ES6), and prototype-based inheritance.
+
+
+
+
+
+
+
+// Certainly, here are the key concepts related to Object-Oriented Programming (OOP) in JavaScript explained in a simple and concise way:
+
+// Objects: Think of objects as containers that hold information and actions, like describing a car with its color and the ability to drive.
+
+// Constructor Functions: They are like templates for creating objects with similar features, such as creating multiple cars with the same properties and abilities.
+
+// Classes (ES6): A newer way to define templates for objects, making it easier to create and manage them.
+
+// Inheritance: Objects can inherit features from other objects, like a new car inheriting traits from an existing car.
+
+// Encapsulation: Imagine putting information and actions related to an object in a neat box, making it easy to use while hiding complex details.
+
+// Abstraction: Simplify complex things by breaking them into smaller, understandable parts, like describing a car without needing to know every detail of how it works.
+
+// Polymorphism: Treating different objects as if they were the same type, making code more flexible.
+
+// Prototype-based Inheritance: Objects can share features by having a "family tree" of properties and methods.
+
+// Composition: Combine smaller objects to build more complex ones, like building a car by putting together its parts.
+
+// Object-Oriented Analysis and Design (OOAD): A way to plan and design software by thinking about the objects, their relationships, and what they can do before writing code.
+
+// In simple terms, OOP in JavaScript helps us organize information and actions in a logical and reusable way, like building with Lego blocks to create things.
