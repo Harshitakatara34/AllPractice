ac486cf773d4983acea7462926a357755240820c
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-20 11:02:55 +0530

    d

---
+++ Questions/Promise.js
@@ -0,0 +1,68 @@
+// let promise=new Promise((res,rej)=>{
+//     let data=true
+//     if(!data){
+//       res("resolve")
+//     }
+//     else{
+//       rej("rejected")
+//     }
+//   })
+//   console.log(promise)
+// Output
+// Promise {<rejected>: 'rejected'}
+// [[Prototype]]
+// : 
+// Promise
+// [[PromiseState]]
+// : 
+// "rejected"
+// [[PromiseResult]]
+// : 
+// "rejected"
+
+
+// #######################################+++++++++++++++++++++#######################################3
+
+///Pending State
+
+// let promise = new Promise((res, rej) => {
+//     // This Promise is intentionally left in a "Pending" state.
+//   });
+  
+//   // Log the Promise object to observe the "Pending" state
+//   console.log("Promise object (Pending):", promise);
+  
+
+
+
+
+//   ###############################################///////////////////##########################
+
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Resolved!"), 1000);
+  });
+  
+  promise.then((value) => console.log(value));
+  
+  setTimeout(() => console.log("Timeout!"), 500);
+  // Timeout
+  // Resolved
+  
+
+
+
+//   ###########################=================================================##################
+
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => reject("Rejected!"), 1000);
+//   });
+  
+//   promise
+//     .then((value) => console.log("Resolved:", value))
+//     .catch((error) => console.log("Error:", error));
+  
+//   setTimeout(() => console.log("Timeout!"), 500);
+//Timeout
+//Error:Rejected
+  
