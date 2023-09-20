e3ccd4a243803ccdd9d2b3a7dfb69ac8b8f14f8f
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-20 11:26:28 +0530

    d

---
+++ Definitions/OOPS.js
@@ -0,0 +1,4 @@
+
+// JavaScript Object-Oriented Programming (OOP) is a programming approach that employs objects to represent 
+// real-world entities, promoting code organization and reusability through 
+// constructor functions, classes (ES6), and prototype-based inheritance.
