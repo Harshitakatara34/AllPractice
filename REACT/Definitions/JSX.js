c708d8cf0e895f090f4eb0e84a80e8c02239e19b
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-09-21 11:17:15 +0530

    done

---
+++ REACT/Definitions/JSX.js
@@ -0,0 +1,10 @@
+// JSX stands for javaScript XML.
+// It allows to write html in react in easier way.
+// 
+
+// example
+
+// JSX code-
+// const element = <h1>Hello, World!</h1>
+
+// const element = React.create("h1", null, 'Hello, World!')
