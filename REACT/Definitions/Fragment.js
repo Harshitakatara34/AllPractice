ab75de42b949fd2408aee81ef87761ff643a2811
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-09-21 20:35:12 +0530

    done

---
+++ REACT/Definitions/Fragment.js
@@ -0,0 +1,11 @@
+// A fragment in React is a way to group elements without introducing an extra DOM element. It's used for cleaner code and better performance, especially when working with lists or multiple elements.
+
+// function App() {
+//     return (
+//       <>
+//         <h1>Hello</h1>
+//         <p>Some text here</p>
+//       </>
+//     );
+//   }
+  
