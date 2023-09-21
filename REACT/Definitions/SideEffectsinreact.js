82fddb879b449e2fcd0eb605191dc51801bd1f4f
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-21 15:09:58 +0530

    d

---
+++ REACT/Definitions/SideEffectsinreact.js
@@ -0,0 +1,9 @@
+//  "side effects" in React refer to any actions or behaviors in a component that interact with the external world, 
+//  such as data fetching, DOM manipulation, timers, and more. React provides the `useEffect` hook to manage these 
+//  side effects in a structured way within functional components.
+
+
+
+
+// side effects refer to similar actions or behaviors that occur alongside the primary logic of a component or function.
+//  React's useEffect hook is a tool to manage such side effects in a structured way within your components.
