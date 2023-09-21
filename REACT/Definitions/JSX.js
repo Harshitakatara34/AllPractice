954a49b42d59dcca7c679ea7676f11767405b4c2
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-21 11:25:51 +0530

    d

--- REACT/Definitions/JSX.js
+++
@@ -1,6 +0,0 @@
-// a JavaScript extension that lets you write HTML-like code in your 
-// JavaScript for building user interfaces, commonly used with 
-// libraries like React.
-
-
-// Example ===>  const element = <h1>Hello, JSX!</h1>;
