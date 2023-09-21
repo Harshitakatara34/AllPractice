350820182cacbe7702b829dde88a84ae72b8646e
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-21 20:27:07 +0530

    d

--- REACT/reactpractice/src/App.js
+++ REACT/reactpractice/src/App.js
@@ -1,14 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import Temp from './Components/Temp';
-import Memooo from './Components/memo';
+import logo from "./logo.svg";
+import "./App.css";
+import Temp from "./Components/Temp";
+import Memooo from "./Components/memo";
+import { Profiler } from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 // import { Data } from './Components/Temp';
 function App() {
+  const onRenderCallback = (
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime
+  ) => {
+    // Log performance data
+    console.log(`Component ${id} took ${actualDuration}ms to render.`);
+  };
+
   return (
     <div className="App">
-    <Temp/>
-    <Memooo/>
-     {/* <Data/> */}
+     
+        <Header />
+        <Profiler id="MyApp" onRender={onRenderCallback}>
+        <Footer />
+        </Profiler>
+        <Memooo />
+     
+
+      {/* <Dta/> */}
     </div>
   );
 }
