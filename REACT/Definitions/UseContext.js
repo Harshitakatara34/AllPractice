6852a01dfb232bbd9f9c4832a7f4f4c62da8b3bd
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-21 18:36:19 +0530

    D

---
+++ REACT/Definitions/UseContext.js
@@ -0,0 +1,3 @@
+// useContext is a React hook used for accessing context values (data) from a higher-level component
+// in a component tree without having to pass props down manually. It simplifies the sharing 
+// of data between components.
