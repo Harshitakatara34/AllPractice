b48473cc2661547c3eff4998688727a70a14dcd0
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-21 16:23:30 +0530

    d

--- REACT/reactpractice/src/App.js
+++ REACT/reactpractice/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Temp from './Components/Temp';
+import Memooo from './Components/memo';
 // import { Data } from './Components/Temp';
 function App() {
   return (
     <div className="App">
     <Temp/>
+    <Memooo/>
      {/* <Data/> */}
     </div>
   );
