75967cfffce3e5fda3fea5e07503783cc79a5957
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-22 21:25:06 +0530

    d

---
+++ REACT/Definitions/CustomHook.js
@@ -0,0 +1,2 @@
+// Custom hooks in React: Reusable functions for sharing logic between components.
+// (Made by own and should start with use )
---
+++ REDUX/Definitions/ComponentRe-renderdiffWAYS.js
@@ -0,0 +1,7 @@
+// 1.State changes
+// 2.Props changes
+// 3.Context Changes
+// 4.Force re-render (By using ForceUpdate)
+// 5.Lifecycle Methods (ShouldComponentUpdate)
+// 6.Hooks
+// 7.Redux(Any state management Library)
