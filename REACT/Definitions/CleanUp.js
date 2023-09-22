3515cffec49154c19fcd24119adf833b853c2ce5
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-23 00:09:20 +0530

    d

---
+++ REACT/Definitions/CleanUp.js
@@ -0,0 +1,6 @@
+
+// The cleanup function in useEffect is used to handle tasks like unsubscribing 
+// from event listeners, clearing timers, or cleaning up resources when a React 
+// component unmounts or when specific dependencies change. It's important to 
+// prevent memory leaks and ensure that no lingering effects interfere with the 
+// component's behavior or cause unexpected issues.
