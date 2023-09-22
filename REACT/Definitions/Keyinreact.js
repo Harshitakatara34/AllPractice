8c2d784dd64f6ed27f9a827690c7f179fbdaf04e
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-22 20:06:15 +0530

    d

--- REACT/Definitions/Keyinreact.js
+++ REACT/Definitions/Keyinreact.js
@@ -1,2 +1,10 @@
 // Keys in React are unique identifiers for elements in lists. They improve performance by helping React update, 
 // reorder, and render list elements efficiently.
+
+// (To avoid re-render)
+
+// Example==>
+let list=['a','b','c'];
+// by key we can identify which element is edited,updated,deleted etc.(it should be unique)
+
+// If we will not give different keys to these elements then they will be re-rendered again and again .
---
+++ REACT/Definitions/StateLessComponentsandStateFullComponent.js
@@ -0,0 +1,10 @@
+// stateless components are simpler and only deal with props and rendering, 
+// while stateful components manage their own internal state and are more complex. 
+// React has shifted toward functional components and hooks for most use cases due to 
+// their simplicity and flexibility.
+
+
+
+
+// StateLess====>Functional Component
+// StateFull===>Class Component
