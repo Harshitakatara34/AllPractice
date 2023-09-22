22f1181e42687f10e65c2225c9d7dc90d6e3b039
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-09-22 06:41:53 +0530

    done

--- Definitions/Protottypical.js
+++ Definitions/Protottypical.js
@@ -1,4 +1,10 @@
 // What is Factory function 
 // The function manufecture and return the object that is called Factory function.
 
-//
+// What is constructor function
+// Any `this` aware function that is intended to be called with the new
+// kwyword is called a constructor function.
+
+// or 
+
+// A constructor function in JavaScript is just a function that makes new objects when you use it with "new." It's like a factory for creating objects.
