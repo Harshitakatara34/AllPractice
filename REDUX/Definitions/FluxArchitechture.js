93c401567bc68ac48872e8c0d94393b37068a3ea
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-22 20:26:01 +0530

    d

---
+++ REDUX/Definitions/FluxArchitechture.js
@@ -0,0 +1,2 @@
+// Flux is an architectural pattern that enforces a one-way data flow, 
+// making it easier to manage data and state in web applications, particularly in larger and more complex projects.
