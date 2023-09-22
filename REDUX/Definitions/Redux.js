176a9589f67acbc9553d754986a6a915e3da219d
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-22 15:08:15 +0530

    f

---
+++ REDUX/Definitions/Redux.js
@@ -0,0 +1,4 @@
+// Redux is a JavaScript library used for managing and controlling the state of a web application. 
+// It's popular because it provides a structured way to handle data, making it easier to develop and
+//  maintain complex applications, especially when combined with libraries like React.
+
---
+++ REDUX/Definitions/WhyRedux.js
@@ -0,0 +1,9 @@
+// Redux is used to:
+
+// Centralize State: Store all application data in one place.
+// Ensure Predictable Updates: Enforce a clear pattern for changing data.
+// Facilitate Debugging: Enable easy tracking of state changes.
+// Support Middleware: Allow custom logic integration. allowing developers to add custom logic, 
+                    //  such as asynchronous operations or logging, to the state management process.
+// Compatibility with React: Redux works seamlessly with React, helping to manage and share state among React components.
+// Scalability: Redux is suitable for handling state in large and complex applications, maintaining code organization as the app grows.
