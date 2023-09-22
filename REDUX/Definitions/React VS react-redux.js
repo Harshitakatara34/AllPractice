ebffd3e2bccfd36f51393c682661ecb37927e497
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-22 21:17:10 +0530

    d

---
+++ REDUX/Definitions/React VS react-redux.js
@@ -0,0 +1,3 @@
+// Redux is a standalone state management library, 
+// while React Redux is a library that connects Redux 
+// with React for easier state management in React applications.
---
+++ REDUX/Definitions/React-redux.js
@@ -0,0 +1,4 @@
+// React Redux: A library that connects React with Redux for efficient state management 
+//             and predictable data flow in applications.
+
+// Example====>useSelector,useDispatch
