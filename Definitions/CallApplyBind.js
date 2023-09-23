48640bf3c90ea460866458ec9610ab35cb3a3030
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-23 09:45:37 +0530

    d

---
+++ Definitions/CallApplyBind.js
@@ -0,0 +1,12 @@
+// .call and .apply are used to invoke a function & set the value of the this keyword inside of the function.
+
+//.call()
+// but .call takes in the value of this keyword as the first argument, rest of the arguments to the function can be sent as comma separated values.
+
+//.apply()
+// but .apply takes in the value of this keyword as the first argument, rest of the arguments to the function can be sent a an array values.
+
+//===========================
+// .bind()
+// .bind() are used to create a function & set the value of the this keyword inside of the newly created function.
+// bind gives you a new function in which the this keyword pre-set for you.
