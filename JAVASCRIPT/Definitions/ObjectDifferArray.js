556fd2777d7d07617301e648f7a8fd623b87d148
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-23 20:48:14 +0530

    d

---
+++ JAVASCRIPT/Definitions/ObjectDifferArray.js
@@ -0,0 +1,4 @@
+// objects are designed for storing data with named attributes (keys), 
+// while arrays are used for ordered lists of values accessed by numerical indices. 
+// The choice between objects and arrays depends on the specific data structure needed 
+// for a given task or use case.
