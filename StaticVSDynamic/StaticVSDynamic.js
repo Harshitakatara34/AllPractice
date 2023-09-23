a7521b0566882e59ac32963c7a10c8902c005b71
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-23 14:51:01 +0530

    D

---
+++ StaticVSDynamic/StaticVSDynamic.js
@@ -0,0 +1,6 @@
+
+// Static Language (e.g., Java): Types are declared before runtime, catching errors early, 
+//                               which makes code more reliable but can be slower to develop.
+
+// Dynamic Language (e.g., Python,Javascript): Types are determined at runtime, allowing for faster development but potentially 
+//                                  leading to runtime errors.
---
+++ Typescript/Definition/Typescript.js
@@ -0,0 +1,3 @@
+// TypeScript is a statically-typed programming language that builds on JavaScript by adding a layer
+//  of type-checking, enabling developers to catch and prevent errors during development,
+//   leading to safer and more maintainable code.
