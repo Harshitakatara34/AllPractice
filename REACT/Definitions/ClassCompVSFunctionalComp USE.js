f5a10047d3dad8920e2cd5433ce5acef888ab639
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-23 09:38:18 +0530

    d

---
+++ REACT/Definitions/ClassCompVSFunctionalComp USE.js
@@ -0,0 +1,11 @@
+// Functional Components:
+// Use for simplicity, reusability, and performance.
+// Ideal for managing state with hooks, using the Context API, and server-side rendering.
+
+
+
+
+// Class Components:
+// Use for complex state management, lifecycle methods, or when working with legacy code.
+// Suitable for ref forwarding, Higher Order Components (HOCs), and inheritance, but consider functional 
+// components and hooks for new projects.
