523064fe086b924594db43fd83865b372305bff5
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-23 22:26:19 +0530

    d

---
+++ JAVASCRIPT/Definitions/Recursion.js
@@ -0,0 +1,16 @@
+// Recursion in JavaScript (Short): Recursion is a technique where a function calls itself to solve a problem. 
+// It involves defining base cases to stop the recursion and recursive cases to call the function with modified 
+// arguments.
+
+
+
+// function factorial(n) {
+//     if (n === 0 || n === 1) {
+//       return 1; // Base case
+//     }
+//     return n * factorial(n - 1); // Recursive case
+//   }
+  
+//   const result = factorial(5); // Calculates 5!
+//   console.log(result); // Outputs 120 (5! = 5*4*3*2*1 = 120)
+  
