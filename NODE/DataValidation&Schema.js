57a8815765b12fa43e21f0713d4165c44f2e3c53
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-24 22:13:37 +0530

    d

---
+++ NODE/DataValidation&Schema.js
@@ -0,0 +1,33 @@
+// In Mongoose, you can handle data validation and schema enforcement like this:
+
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 20,
+  },
+  age: {
+    type: Number,
+    min: 18,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+const newUser = new User({
+  username: 'john_doe',
+  age: 25,
+});
+
+newUser.save()
+  .then((user) => console.log('User saved:', user))
+  .catch((err) => console.error('Validation error:', err.message));
+
+
+//   In this example, we define a schema for a user with rules like "username must be between 4 and 20 characters" 
+//   and "age must be at least 18." Mongoose enforces these rules when saving data, ensuring that it meets the specified 
+//   criteria.
