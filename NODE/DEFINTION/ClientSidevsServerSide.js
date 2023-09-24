a9cf0140c5c404f4d7df0c5e19adfb3eb9cbd5a5
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-24 10:24:32 +0530

    d

---
+++ NODE/DEFINTION/ClientSidevsServerSide.js
@@ -0,0 +1,5 @@
+// Client-side programming: Code that runs in the user's web browser, 
+//             responsible for user interface and interactions.
+
+// Server-side programming: Code that runs on the web server, handling requests, 
+//                         processing data, and executing business logic.
---
+++ NODE/DEFINTION/Nodejswhyimp.js
@@ -0,0 +1,3 @@
+// Node.js is a runtime environment that allows developers to use JavaScript for server-side programming. 
+// It's popular because it's fast, efficient, and enables non-blocking, event-driven programming, making it 
+// ideal for building scalable and real-time server applications.
