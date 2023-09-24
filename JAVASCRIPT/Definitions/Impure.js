442ade33930257612ac727ef4896b8d141bb183f
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-24 22:30:18 +0530

    d

---
+++ JAVASCRIPT/Definitions/Impure.js
@@ -0,0 +1,14 @@
+// Impure Function: Can produce different outputs for the same input or has side effects that affect external state.
+
+// Impure function
+// let total = 0;
+
+// function addToTotal(value) {
+//   total += value;
+// }
+
+// addToTotal(5);
+// console.log(total); // Output: 5
+
+// addToTotal(3);
+// console.log(total); // Output: 8 (Different output for the same function call)
---
+++ JAVASCRIPT/Definitions/PureFunction.js
@@ -0,0 +1,10 @@
+// Pure Function: Always produces the same output for the same input and has no side effects.
+
+// Pure function
+// function add(a, b) {
+//     return a + b;
+//   }
+  
+//   console.log(add(2, 3)); // Output: 5
+//   console.log(add(2, 3)); // Output: 5 (Always the same output for the same inputs)
+  
