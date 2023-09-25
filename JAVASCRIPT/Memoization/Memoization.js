e9214b38646cef2fc8d56c913af67a8c27108e3b
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-09-25 10:40:53 +0530

    done

---
+++ JAVASCRIPT/Memoization/Memoization.js
@@ -0,0 +1,39 @@
+// Implementing Momoization in JavaScript.
+// Memoization is an optimization technique that can be used to reduce
+//time-consuming calculation by saving previous input to something called cache and 
+//returing the result from it.
+
+
+let sum = 0;
+
+const calc = (n)=>{
+    for(let i=0; i<=n; i++){
+        sum+=i;
+    }
+    return sum;
+}
+console.log(calc(5))
+
+
+const memoize = (fun) =>{
+    let cache = {};
+
+    return function(...args){
+        let n = args[0]
+        if(n in cache){
+            console.log('cache')
+            console.log(cache)
+            return cache[n];
+        }else{
+            console.log('calculating first time')
+            let result = fun(n)
+            cache[n] = result;
+            return result;
+        }
+    }
+}
+
+console.time();
+const efficient = memoize(calc);
+console.log(efficient(5))
+console.timeEnd();
