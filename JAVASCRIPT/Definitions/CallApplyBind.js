6b5dd6a5dcde9193170cf94d47b185f7fff2576e
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-09-25 09:01:05 +0530

    done

--- JAVASCRIPT/Definitions/CallApplyBind.js
+++ JAVASCRIPT/Definitions/CallApplyBind.js
@@ -10,3 +10,9 @@
 // .bind()
 // .bind() are used to create a function & set the value of the this keyword inside of the newly created function.
 // bind gives you a new function in which the this keyword pre-set for you.
+
+
+//=======================
+// Difference between call, apply and bind
+
+// call and apply does imediatily invoke a function (argument lete hai, function invoke karte hai, jo return hai value dete hai) or bind is create a new function you can used it any where in a code.
