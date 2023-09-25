f143f7d951b5459d182cf1700e4f2d7fbb80227c
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-25 10:49:08 +0530

    d

--- JAVASCRIPT/Memoization/Memoization.js
+++ JAVASCRIPT/Memoization/Memoization.js
@@ -5,19 +5,23 @@
 
 
 let sum = 0;
-
+// Without Memoization
 const calc = (n)=>{
     for(let i=0; i<=n; i++){
         sum+=i;
     }
     return sum;
 }
+
+console.time()
 console.log(calc(5))
+console.timeEnd()
 
 
+
+//With Memoization
 const memoize = (fun) =>{
     let cache = {};
-
     return function(...args){
         let n = args[0]
         if(n in cache){
@@ -32,8 +36,8 @@ const memoize = (fun) =>{
         }
     }
 }
-
 console.time();
 const efficient = memoize(calc);
+console.log(efficient(5))
 console.log(efficient(5))
 console.timeEnd();
