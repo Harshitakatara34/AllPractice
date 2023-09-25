20a3bb32d8312eac3836473ff60333aeb5ce8d97
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-25 09:54:30 +0530

    d

---
+++ NODE/Hashing/Salt Rounds.js
@@ -0,0 +1,3 @@
+// Generate a Salt: A random "salt" is generated. A salt is a random value that is combined with 
+//                  the password before hashing. It adds an extra layer of security by ensuring that even if two users 
+//                  have the same password, their hashed passwords will be different due to the unique salt.
