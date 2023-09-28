676e66d9990af9e3c79a8d1b877a3bdef98275a7
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-09-28 17:28:33 +0530

    d

--- JAVASCRIPT/Definitions/RestApi.js
+++ JAVASCRIPT/Definitions/RestApi.js
@@ -1,2 +1,8 @@
 // REST API (Very Short): It's a web API that uses standard HTTP methods and URLs for 
 //                        communication between clients and servers.
+
+
+
+
+// "API" is a broad term for any interface that enables communication between software components.
+// "REST API" is a type of API that uses specific principles and HTTP methods for communication.
--- JAVASCRIPT/Definitions/This.js
+++ JAVASCRIPT/Definitions/This.js
@@ -1,1 +1,6 @@
 // "this" refers to the current context or object, and its meaning can vary based on the code's context.
+
+
+// 1.Global Context
+// 2.Implicit  (by obj)
+// 3.Explicit (call,apply)
