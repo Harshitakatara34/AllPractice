4cffe72a66d6e3435987fbc372db59d6dc226ef4
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-09-29 13:40:01 +0530

    done

--- JAVASCRIPT/ExtraConcept/Console/ConsoleOperator.js
+++ JAVASCRIPT/ExtraConcept/Console/ConsoleOperator.js
@@ -1,8 +1,23 @@
-// console.log('2'+1) // 21
-// console.log('2'-1)  // 1
-// console.log('2'*1) // 2
-// console.log('2'/1) // 2
+// let a = '2'+1
+// console.log(a)
+// console.log(typeof a) // string
 
+//========================
+
+// let b = '2'-1
+// console.log(b)  // 1
+// console.log(typeof b) // number
+
+// //====================
+// let c = '2'*1
+// console.log(c)  // 2
+// console.log(typeof c) // number
+
+// //=======================
+
+// let d = '2'/1
+// console.log(d) // 2
+// console.log(typeof d) // number
 
 // it will work concatination in strings when positive numbers like (+1, +2 etc.) 
 // other any operattions like *1, -1, /1 etc. works as opetators.
