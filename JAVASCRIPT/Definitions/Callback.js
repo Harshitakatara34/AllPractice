bccc7cc04063692a84b1e34fb58cac58a4406ef6
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-09-29 19:22:06 +0530

    done

--- JAVASCRIPT/Definitions/Callback.js
+++ JAVASCRIPT/Definitions/Callback.js
@@ -1,6 +1,10 @@
+
+// A callback function is a function passed as an argument to another function, to be executed later in response to an event or as part of asynchronous programming.
+
+
+
 // callback functions are used to delay the execution 
 // of a function until a specific condition is met or an event occurs.
-
 
 
 
---
+++ Samreen Js Practice/PracticeForInterview/EventHandler.js
@@ -0,0 +1,20 @@
+//html
+
+{/* <html>
+  <head></head>
+  <body>
+    <button id='btn'>Add</button>
+  </body>
+</html> */}
+
+
+//===============
+
+//js
+
+// let btn = document.getElementById("btn");
+
+// btn.addEventListener('click', ()=>{
+//   console.log('Click Me')   // "Click Me"
+// })
+  
