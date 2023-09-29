b8dc8d6ee1242387cb64056126be3723d2be0a04
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-09-29 12:04:26 +0530

    done

--- Console/ConsoleOperator.js
+++ Console/ConsoleOperator.js
@@ -3,5 +3,6 @@
 // console.log('2'*1) // 2
 // console.log('2'/1) // 2
 
+
 // it will work concatination in strings when positive numbers like (+1, +2 etc.) 
 // other any operattions like *1, -1, /1 etc. works as opetators.
