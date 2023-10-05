283405b12b3ad2544569016625076c6c10748aa6
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-10-05 23:58:42 +0530

    done

--- Samreen Practice/PracticeForInterview/JavaScript Practice/MCQ_JS_Practice.jsx/callbindQuestions.js
+++ Samreen Practice/PracticeForInterview/JavaScript Practice/MCQ_JS_Practice.jsx/callbindQuestions.js
@@ -101,3 +101,70 @@
   
 //   greetEnglish(); // Should output "Hello, John!"
 //   greetSpanish(); // Should output "¡Hola, Juan!"
+
+//==========================================================
+
+// function calculate(a, b, operation) {
+//     return operation.call(null, a, b);
+//   }
+  
+//   function add(x, y) {
+//     return x + y;
+//   }
+  
+//   function subtract(x, y) {
+//     return x - y;
+//   }
+  
+//   const result1 = calculate(5, 3, add);
+//   const result2 = calculate(10, 4, subtract);
+  
+//   console.log(result1); // Should output 8
+//   console.log(result2); // Should output 6
+
+
+
+//===================================
+
+// function printPerson() {
+//     console.log(`${this.firstName} ${this.lastName}, ${this.age} years old`);
+//   }
+  
+//   printPerson.call({
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30,
+//   })
+  
+// Should print "John Doe, 30 years old"
+  
+//==============================
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30,
+//   };
+  
+//   function printPerson(person) {
+//     console.log(`${person.firstName} ${person.lastName}, ${person.age} years old`);
+//   }
+  
+//   printPerson(person); // Should print "John Doe, 30 years old"
+  
+
+//==============================================
+
+// const product = {
+//     name: "Laptop",
+//     price: 1000,
+//     description: "A powerful laptop",
+//     discount: function (percentage) {
+//       return this.price - (this.price * percentage) / 100;
+//     },
+//   };
+  
+//   const discountedPrice = product.discount(10);
+//   console.log(discountedPrice); // Should return 900 (10% discount)
+  
+//=====================================================
