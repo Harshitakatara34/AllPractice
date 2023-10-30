70ed004f42d06b2189ad40ac6488e0840f4e6f1b
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-10-30 11:21:57 +0530

    done

--- Samreen Practice/PracticeForInterview/React Practice/react-interview-questions-coding-rounds/src/Components/Vote.jsx
+++ Samreen Practice/PracticeForInterview/React Practice/react-interview-questions-coding-rounds/src/Components/Vote.jsx
@@ -1,25 +1,65 @@
-import { useState } from "react";
+// import React, { useState } from 'react';
 
-export const Vote = () => {
-  const [age, setAge] = useState(5);
+// function Vote() {
+//   const [age, setAge] = useState(0);
+//   const [canVote, setCanVote] = useState(false);
 
-  const handleChange = () => {
-    if (age>=18) {
-      setAge(true);
-      alert('Vote')
+//   const handleAgeChange = (e) => {
+//     const newAge = parseInt(e.target.value);
+//     setAge(newAge);
+//     setCanVote(newAge >= 18);
+//   };
+
+//   return (
+
+//     <div>
+//       <br/>
+//       <hr/>
+//       <br/>
+//       <h1>Can You Vote?</h1>
+//       <label>
+//         Enter your age:
+//         <input type="number" value={age} onChange={handleAgeChange} />
+//       </label>
+//       <p>
+//         {canVote
+//           ? "You are eligible to vote."
+//           : "You are not eligible to vote."
+//         }
+//       </p>
+//     </div>
+//   );
+// }
+
+// export default Vote;
+
+//======================
+
+import React, { useEffect, useState } from "react";
+
+function Vote() {
+  const [agevote, setagevote] = useState("");
+
+  let age = 1;
+
+  useEffect(()=>{
+    if (age >= 18) {
+      setagevote("Voting");
     } else {
-      setAge(false);
-      alert(`Not vote`)
+      setagevote("not Voting");
     }
-  };
+  },[age])
+
+
 
   return (
     <>
       <br />
       <hr />
-      <h1>Voting</h1>
       <br />
-      <h1 onClick={handleChange}>Voting:{age}</h1>
+      <h1>Vote: {agevote}</h1>
     </>
   );
-};
+}
+
+export default Vote;
