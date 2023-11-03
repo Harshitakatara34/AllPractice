90a54d388cb27a501bddb0686fd07956415f9c24
Summi51 <73363392+Summi51@users.noreply.github.com>
2023-11-04 00:39:58 +0530

    done

--- DSA/DSA_QUESTIONS_SOLUTIONS_BASIC_IMPORTANT.js
+++ DSA/DSA_QUESTIONS_SOLUTIONS_BASIC_IMPORTANT.js
@@ -166,6 +166,36 @@
 // pattern(4)
 
 
+//====================================
+
+//Sum ADD in Array 
+
+
+// CREATE an new ARRAY OF SUMS : At any INDEX put SUM of all elements expect that INDEX element 
+
+// const input = [2, 7, 11, 4, -2] 
+// const output = [20, 15, 11, 18, 24]  
+
+// function ArraySum(input){
+//     let output = []
+//     for(let i=0; i<input.length; i++){
+//       let sum = 00;
+      
+//       for(let j=0; j<input.length; j++){
+        
+//         if(j!==i){
+//           sum = sum + input[j]
+//         }
+//       }
+//       output.push(sum)
+//       console.log(output)
+//     }
+//   }
+  
+  
+//   ArraySum([2, 7, 11, 4, -2])
+  
+
 // =================================
 
 // Patterns Program
